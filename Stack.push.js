2fe10e17fdbc2202cd7eb9ab78632f8798431d15
Piyush <94220242+Piyush-Meena@users.noreply.github.com>
2021-11-28 06:50:43 +0530

    Stack.push.js

---
+++ Stack.push.js
@@ -0,0 +1,16 @@
+let stack = [];
+
+stack.push(1);
+console.log(stack); // [1]
+
+stack.push(2);
+console.log(stack); // [1,2]
+
+stack.push(3);
+console.log(stack); // [1,2,3]
+
+stack.push(4);
+console.log(stack); // [1,2,3,4]
+
+stack.push(5);
+console.log(stack); // [1,2,3,4,5]
